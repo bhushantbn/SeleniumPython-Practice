0b8c77889b9582ea1d65b62765460cd724f0e47a
bhushantbn <bhushantbn@gmail.com>
2025-08-17 11:58:50 +0530

    RadioCondition1.py

--- RadioCondition1.py
+++ RadioCondition1.py
@@ -3,27 +3,29 @@
 # This script uses Selenium to test radio buttons on a webpage.
 # It verifies the total number of radio buttons and the number of disabled radio buttons.
 
-
-
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 # Set up headless options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--window-size=1920,1080")
+
 
 # Create the WebDriver instance
-driver = webdriver.Chrome(options=chrome_options)
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.maximize_window()
+wait = WebDriverWait(driver, 20)
 
 driver.get("https://testing.qaautomationlabs.com/radio-button.php")
-
-radioButton=driver.find_element(By.LINK_TEXT,"Radio Button")
-radioButton.click()
 
 radioCount=driver.find_elements(By.XPATH, "//input[@type='radio']")
 
